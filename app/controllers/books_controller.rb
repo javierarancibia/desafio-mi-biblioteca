e575699c0badfc392fffc895bb4ebbb69100aa9b
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-20 21:08:12 -0300

    CREATE: form and create method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
-    before_Action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
     def index
         @books = Book.all
     end
 
+    def new
+        @book = Book.new 
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path, notice: '¡El libro fue creado con exito!'
+        else
+            flash.now[:alert] = 'Lamentablemente el libro no pudo ser creado'
+            render :new
+        end
+    end
+
     private
     def set_book
         @book = Book.find(params[:id])
+    end
+
+    def book_params
+        params.require(:book).permit(:title, :description, :author, :status, :date_out, :date_back)
     end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true
-    validates :description, presence: true, length { maximum: 120}
+    validates :description, presence: true, length: { maximum: 120}
     validates :author, presence: true
     validates :date_out, presence: true
     validates :date_back, presence: true
